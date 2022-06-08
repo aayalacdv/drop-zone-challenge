8cb5f1d22a9256af6f680fecbf7b0bed8522019f
aayalacdv <aayalacdv2000@gmail.com>
2022-06-08 09:17:41 +0200

    made design responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App flex justify-center items-center'>
+    <div className='App flex flex-col sm:flex-row justify-center items-center '>
       <div className='bg-red-500 h-full w-full flex items-center justify-center'>
         <div className='w-3/4 h-1/2 border border-black rounded-md'>
           <p>Input box </p>
         </div>
       </div>
-      <div className='bg-blue-200 h-full w-3/4 flex items-center justify-center'>
+      <div className='bg-blue-200 h-full sm:w-3/4 w-full flex items-center justify-center'>
         <div className='w-3/4 h-1/2 border border-black'>
           <div className='w-full h-3/4 bg-red-100'>Text</div>  
           <div>buttons</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '700px',
+    }, 
     extend: {},
   },
   plugins: [],
