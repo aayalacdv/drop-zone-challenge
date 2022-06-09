82419ab2bb14f1c621de9cdaa0c15b556f9200c4
aayalacdv <aayalacdv2000@gmail.com>
2022-06-09 06:19:29 +0200

    implemented input of type file

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App flex flex-col sm:flex-row justify-center items-center '>
-      <div className='bg-red-500 h-full w-full flex items-center justify-center relative'>
-        <div className='w-4/5 h-3/5  bg-green-600 blur-md absolute z-0'/>
-        <div className='z-10 w-3/4 h-1/2 rounded-md bg-black'>Hola que tal</div>
+    <div className='flex flex-col items-center justify-center App sm:flex-row '>
+      <div className='relative flex items-center justify-center w-full h-full bg-red-500'>
+        <div className='absolute z-0 w-4/5 bg-green-600 h-3/5 blur-md'/>
+        <div className='z-10 flex items-center justify-center w-3/4 bg-black h-1/2 rounded-md hover:cursor-pointer hover:bg-gray-200'>
+            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full cursor-pointer">
+                <input id="dropzone-file" type="file" className="hidden" />
+            </label>
+        </div>
       </div>
-      <div className='bg-blue-200 h-full sm:w-3/4 w-full flex items-center justify-center'>
-        <div className='w-3/4 h-1/2 border border-black '>
-          <div className='w-full h-3/4 bg-red-100'>Text</div>  
+      <div className='flex items-center justify-center w-full h-full bg-blue-200 sm:w-3/4'>
+        <div className='w-3/4 border border-black h-1/2 '>
+          <div className='w-full bg-red-100 h-3/4'>Text</div>  
           <div>buttons</div>
         </div>
       </div>
