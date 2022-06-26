dd1c99b283b08e350c50d6e5788d2d113c018c88
aayalacdv <aayalacdv2000@gamil.com>
2022-06-26 13:57:06 +0200

    added svg logos for the win :)

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useCallback } from "react";
 import { useDropzone } from 'react-dropzone';
+import LogosCanvas from "./LogosCanvas";
+
+
+
+
+
 
 
 export const DropZone: React.FC = () => {
@@ -33,6 +39,7 @@ export const DropZone: React.FC = () => {
                 className='relative flex items-center justify-center w-full h-1/2'>
                 <div className='absolute z-0 w-[90%] bg-green h-full blur-md' />
                 <div className='z-10 flex items-center justify-center w-[88%] bg-purple h-[98%] rounded-md hover:cursor-pointer'>
+                    <LogosCanvas/>
                     <label
                         onMouseEnter={(e) => setMouseEnter((prev) => true)}
                         onMouseLeave={(e) => setMouseEnter((prev) => false)}
@@ -41,7 +48,7 @@ export const DropZone: React.FC = () => {
                         className="flex flex-col items-center justify-center w-full h-full cursor-pointer">
 
                         {(loaded == false && uploaded == false) || (loaded == true && uploaded == false) ?
-                            <p className="text-xl text-white text-center">ARRASTRA TUS ARCHIVOS AQUÍ</p> :
+                            <p className="text-4xl text-white text-center">ARRASTRA TUS ARCHIVOS AQUÍ</p> :
                             <p className="text-xl text-white text-center">
                                 {files.length == 1 ?
                                     `TU ARCHIVO ${files[0]} SE HA SUBIDO CORRECTAMENTE` :
